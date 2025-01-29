3f3b5ce4ec30ac44476980a159be3a143808f277
Danyal Khanzada <danyalkhanzada@Danyals-MacBook-Air.local>
2025-01-28 22:38:29 -0500

    public route table

--- variables.tf
+++ variables.tf
@@ -8,3 +8,13 @@ variable "private_subnet_ids" {
   description = "IDs of private subnets for ASG"
   type        = list(string)
 }
+
+# Public Route Table- All traffic
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"  
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = { Name = "public-rt" }
+}
